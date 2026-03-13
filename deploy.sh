12726803cf1026e5b32d7c19c580a52ad602fe26
Jeffrey Hein <jp@jphein.com>
2026-03-13 12:49:48 -0700

    Add deploy script to prevent symlink deletion

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Deploy extension from source to GNOME Shell
+SRC="$HOME/Projects/hostname-in-title"
+DEST="$HOME/.local/share/gnome-shell/extensions/hostname-in-title@local"
+
+rm -rf "$DEST"
+cp -r "$SRC" "$DEST"
+rm -rf "$DEST/.git" "$DEST/.gitignore" "$DEST/deploy.sh" "$DEST/README.md"
+echo "Deployed. Log out/in to reload."
